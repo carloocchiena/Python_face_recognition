18777b859f2fbe83b68bc5ca9b994ed5a936d6a9
Carlo Occhiena <57464184+carloocchiena@users.noreply.github.com>
2021-02-10 16:26:45 +0100

    Create dinamic.py

---
+++ dinamic.py
@@ -0,0 +1,53 @@
+import cv2
+import sys
+import logging as log
+import datetime as dt
+from time import sleep
+
+cascPath =cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+log.basicConfig(filename='webcam.log',level=log.INFO)
+
+video_capture = cv2.VideoCapture(0)
+anterior = 0
+
+while True:
+    if not video_capture.isOpened():
+        print('Unable to open webcam. Program terminated within 10 sec')
+        sleep(10)
+        pass
+
+    # Capture frame-by-frame & transform in grayscale
+    ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=6, #lower = faster, less accurate. Good range: 3-6
+        minSize=(30, 30)
+    )
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    if anterior != len(faces):
+        anterior = len(faces)
+        log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
+
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
